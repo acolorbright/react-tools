a361408875d36890c07c0120178234cad8df3eba
tomislav-s <tomislav@acolorbright.com>
2021-08-13 10:13:55 +0200

    feat: add axe react hook

---
+++ packages/react-hooks/hooks/useAxeReact.ts
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
+
+export const useAgnosticAxe = (): void => {
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'production') {
+      // eslint-disable-next-line import/no-extraneous-dependencies
+      import('@axe-core/react').then((axe) =>
+        axe.default(React, ReactDOM, 1000)
+      )
+    }
+  }, [])
+}
